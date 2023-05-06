b2f0147d9205eff9531bd1f09d923ea2eb50c11c
matansol <matan.solomo@gmail.com>
2023-05-06 15:55:14 +0300

    part1 and 2 answers

--- hw2_spring_23/hw2/answers.py
+++ hw2_spring_23/hw2/answers.py
@@ -11,6 +11,15 @@ math (delimited with $$).
 part1_q1 = r"""
 **Your answer:**
 
+1. 
+A. the shape of the tensor is [N x in_features x N x out_features] = [64 x 1024 x 64 x 512]
+B. Yes, most of the elements are zeros, all the cells [i][j][d][f] where $j != f$
+
+
+2.
+A. the shape of the tensor is [in_features x out_features x N x out_features] = [1024 x 512 x 64 x 512]
+B. Yes, most of the elements are zeros, all the cells [i][j][d][f] where $j != f$
+
 
 Write your answer using **markdown** and $\LaTeX$:
 ```python
@@ -24,6 +33,7 @@ An equation: $e^{i\pi} -1 = 0$
 part1_q2 = r"""
 **Your answer:**
 
+No, backprop is not required for training neural networks, after bulding the network we can calculate the gradient for each parameter separately without using the chain rule. the problems with that method is that we will need to calculate for the gradients again every time we changing our network and it much slower because we calculate the same calculating for many parameters.
 
 Write your answer using **markdown** and $\LaTeX$:
 ```python
@@ -80,13 +90,14 @@ def part2_optim_hp():
 
 def part2_dropout_hp():
     wstd, lr, = (
-        0.1,
-        0.003,
+        0,
+        0,
     )
     # TODO: Tweak the hyperparameters to get the model to overfit without
     # dropout.
     # ====== YOUR CODE: ======
-    #raise NotImplementedError()
+    wastd = 0.2
+    lr = 0.01
     # ========================
     return dict(wstd=wstd, lr=lr)
 
@@ -107,6 +118,9 @@ An equation: $e^{i\pi} -1 = 0$
 part2_q2 = r"""
 **Your answer:**
 
+yes it is possible, because the cross-entropy function is a measure of the difference between the predicted probability distribution and the actual probability distribution and not the numbers that the model predict worng.
+there can be a situation were the biggest probability of some samples is changing witch make the accuracy to increse but in the same time the probability of other calss is changing to and making the cross-entropy loss to increas.
+
 
 Write your answer using **markdown** and $\LaTeX$:
 ```python
@@ -119,6 +133,29 @@ An equation: $e^{i\pi} -1 = 0$
 
 part2_q3 = r"""
 **Your answer:**
+
+1. GD is an learning algorithm for getting to the minimum loss by stepping small stepts for the oposite direction of the gradient in each point. GD using the gradient for the algirithm, GD don't tell us how to calculate the gradients.
+backprop is a relative fast way to calculate the gradient for each parameter means $\delta L/\delta p$ when L is the loss function and p in the parameter.
+
+2. the difference is that in GD for each step we calculate the parameters gradients on all our sampels and sum them up.
+but in SDG we on each step we calculate the gradients on a 1 sample from the data and updating our params base on that gradients.
+each step in GD in more accurate toward the minimum that we want to get to, but calculate the gradients on all the sample take a long long time. 
+
+
+3. like we said before SGD is much faster espessaly in Deep Learning when we are training on **ALOT** of samples so it is almost impossible to use GD as it is. 
+another reason is that in DL sometimes our samples are very big and we can't laod are all data samples to the computer RAM so it will take even longer to calculate the gradient on all the samples.   
+as we seen in calss the loss functions in DL are not convex so they have a local minimun points, by using SGD we dont go directly to the minimum and that helps avoiding those local minimum points.
+
+4. 
+A. Yes, in both options the gradient will be the same.
+In GD we do a forward pass with all the samples and sum up the loss form all the samples and then backprop and calculate the gradients, in the new method we do a forward pass on baches of the samples and then sum up the loss. 
+
+Knowing that loss is a linear operation we can say that: 
+$(\sum \nabla L = \nabla \sum L)$ and therefore both expression are Equivalent.
+So the backprop is on the same loss sum so we will get the same gradients.
+
+B.  To calculate the gradient in each layer we need the gradients from the next layer that we get using backprop, and we also need the forward calculation on the layer that we storage in memory for each layer for each batch. because we have a lot of layers and a lot of samples even after doing forward on each batch separatly we still need to keep the result on each layer for all the batches so the computer memory will run out of space. 
+
 
 
 Write your answer using **markdown** and $\LaTeX$:
@@ -157,10 +194,7 @@ def part3_arch_hp():
     out_activation = "none"  # activation function to apply at the output layer
     # TODO: Tweak the MLP architecture hyperparameters.
     # ====== YOUR CODE: ======
-    n_layers = 4
-    hidden_dims = 8
-    activation = "relu"
-    out_activation = "relu"
+    raise NotImplementedError()
     # ========================
     return dict(
         n_layers=n_layers,
@@ -182,10 +216,7 @@ def part3_optim_hp():
     #    What you returns needs to be a callable, so either an instance of one of the
     #    Loss classes in torch.nn or one of the loss functions from torch.nn.functional.
     # ====== YOUR CODE: ======
-    lr = 0.01
-    weight_decay = 0.005
-    momentum = 0.7
-    loss_fn = torch.nn.CrossEntropyLoss()
+    raise NotImplementedError()
     # ========================
     return dict(lr=lr, weight_decay=weight_decay, momentum=momentum, loss_fn=loss_fn)
 
