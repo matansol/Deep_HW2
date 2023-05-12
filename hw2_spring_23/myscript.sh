d097db5b1834193e8921c4a915283a2b0845c2f6
Matan-Itamar Solomon <ma.solomon@lambda.csf.technion.ac.il>
2023-05-12 16:44:22 +0300

    sbatch script

---
+++ hw2_spring_23/myscript.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Setup env
+source $HOME/miniconda3/etc/profile.d/conda.sh
+conda activate cs236781-hw
+
+# Define the range of values for L and K
+L_values=(2 4 8 16)
+K_values=(32 64)
+
+# Loop through the values
+for K in "${K_values[@]}"; do
+  for L in "${L_values[@]}"; do
+    # Assemble the command with the current L and K values
+    command="srun -c 2 --gres=gpu:1 --pty python -m hw2.experiments run-exp -n exp1_1_L$L_K$K -K $K -L $L -P 2 -H 100"
+
+    # Execute the command
+    eval "$command"
+  done
+done
